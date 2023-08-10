c85a3c487e14990e28490f8febf55090fb8483c6
amedzaphilips <danielamedzaphilips@gmail.com>
2023-08-10 10:15:32 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints an output and a new line.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
