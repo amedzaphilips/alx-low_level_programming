fbbad0a5d07c7c96acbc308df21a62a26e18e633
amedzaphilips <danielamedzaphilips@gmail.com>
2023-08-13 12:45:22 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints some alphabets
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+	{
+		if (a == 101 || a == 113)
+		{	a++;
+			continue;
+		}
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+}
