0765b2c0baeac2b8366bb957dc8529845ff1d4f1
amedzaphilips <danielamedzaphilips@gmail.com>
2023-08-20 16:57:36 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints _puchar
+ *
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar(10);
+	return (0);
+}
